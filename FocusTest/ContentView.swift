9b64d0ba0e9f5dc59940515784d584235e26049b
Steven Fisher <tewha@me.com>
2021-10-11 11:38:24 -0700

    Simplify by removing scroll view.

--- FocusTest/ContentView.swift
+++ FocusTest/ContentView.swift
@@ -12,19 +12,18 @@ struct ContentView: View {
     @State var name: String = ""
 
     var body: some View {
-        ScrollView {
-            VStack (alignment: .leading, spacing: 10) {
-                Text("We're going to try to focus on this field.")
-                TextField("Name", text: $name)
-                    .focused($isNameFocused)
-                    .onAppear {
-                        self.isNameFocused = true
-                    }
-                    .task {
-                        self.isNameFocused = true
-                    }
-            }
-        }
+        VStack (alignment: .leading, spacing: 10) {
+            Text("We're going to try to focus on this field.")
+            TextField("Name", text: $name)
+                .focused($isNameFocused)
+                .onAppear {
+                    self.isNameFocused = true
+                }
+                .task {
+                    self.isNameFocused = true
+                }
+            Spacer()
+        }.padding()F
     }
 }
 
