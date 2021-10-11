abad06875294b735eb7bd38285973f0f23a1105d
Steven Fisher <tewha@me.com>
2021-10-10 18:45:46 -0700

    Add project template.

---
+++ FocusTest/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  FocusTest
+//
+//  Created by Steven Fisher on 2021-10-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @FocusState private var isNameFocused: Bool
+    @State var name: String = ""
+
+    var body: some View {
+        ScrollView {
+            VStack (alignment: .leading, spacing: 10) {
+                Text("We're going to try to focus on this field.")
+                TextField("Name", text: $name)
+                    .font(.headline)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+                    .focused($isNameFocused)
+                    .onAppear {
+                        self.isNameFocused = true
+                    }
+                    .task {
+                        self.isNameFocused = true
+                    }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ FocusTest/FocusTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTestApp.swift
+//  FocusTest
+//
+//  Created by Steven Fisher on 2021-10-10.
+//
+
+import SwiftUI
+
+@main
+struct FocusTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
